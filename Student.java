81c8bdf457d3c9156f436229b0551f9d5a8e185b
大漠西风起 <32919605+1341005547@users.noreply.github.com>
2018-01-28 13:19:48 +0800

    Add files via upload
    lombokTest测试代码

---
+++ Student.java
@@ -0,0 +1,15 @@
+package lombokdemo;
+
+import lombok.*;
+
+/**
+ * Created by Administrator on 2018/1/28.
+ */
+@Data
+public class Student {
+
+    private String name;
+    private int age;
+    private String male;
+    private String studentNo;
+}
